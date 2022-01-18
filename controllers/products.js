bcf7db65708315bb4be256e4b0c7d53f8329888a
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-18 09:18:57 +0530

    added query builers and validators

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,14 @@
 const Product = require("../models/Product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ar";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({price:{$gt: 30}})
+	.select('name company price')
+	.sort('price')
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields} = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -20,8 +19,27 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  const products = await Product.find(queryObject);
-  res.status(200).json({ products, nbHits: products.length });
+
+  let result = Product.find(queryObject);
+	// sort 
+	if (sort){
+		const sortList = sort.split(',').join(' ')
+		result = result.sort(sortList)
+	}
+	else{
+		result = result.sort('createdAt')
+	}
+	// fields
+	if (fields){
+		const fieldList = fields.split(',').join(' ')
+		result = result.select(fieldList)
+	}
+	const page = Number(req.query.page) || 1
+	const limit = Number(req.query.limit) || 10
+	const skip = (page-1) * limit
+
+	const products = await result.limit(limit).skip(skip)
+  res.status(200).json({ nbHits: products.length, products  });
 };
 
 module.exports = {
